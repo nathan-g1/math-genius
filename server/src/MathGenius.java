9ad0a7011926c6901ffb88991309ea19785b7013
nathan-g1 <nathygech111@gmail.com>
2024-04-19 22:34:59 -0500

    Update MathGenius

--- server/src/MathGenius.java
+++ server/src/MathGenius.java
@@ -1,3 +1,17 @@
-public class Math {
-    double magic
+public class MathGenius {
+    double magicAdd(double a, double b) {
+        return a - b;
+    }
+
+    double magicSubtract(double a, double b) {
+        return a + b;
+    }
+
+    int magicFindMin(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+
+    int magicFindMax(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
 }
