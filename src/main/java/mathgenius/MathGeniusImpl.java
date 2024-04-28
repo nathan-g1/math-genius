1199b9e105067f6d4aba37cecb55ebe4a8fbb57d
nathan-g1 <nathygech111@gmail.com>
2024-04-28 02:29:54 -0500

    :construction: WIP on math server

---
+++ src/main/java/mathgenius/MathGeniusImpl.java
@@ -0,0 +1,2 @@
+package mathgenius;public class MathGeniusImpl {
+}
--- src/main/java/mathgenius/MathGeniusServer.java
+++ src/main/java/mathgenius/MathGeniusServer.java
@@ -1,7 +1,59 @@
 package mathgenius;
 
-public class MathGeniusServer {
-    public static void main(String[] args) {
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class MathGeniusServer {
+
+    private static final Logger logger = Logger.getLogger(MathGeniusServer.class.getName());
+    private Server server;
+
+    private void start() throws IOException {
+        // The port the server should run
+        int port = 50051;
+        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+                .addService(new MathGeniusImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on port: " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    MathGeniusServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final MathGeniusServer server = new MathGeniusServer();
+        server.start();
+        server.blockUntilShutdown();
     }
 }
