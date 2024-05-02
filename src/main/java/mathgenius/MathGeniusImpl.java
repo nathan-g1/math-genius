8d22a2d26b0cc282d0f7dcf31624a57f35539427
nathan-g1 <nathygech111@gmail.com>
2024-05-02 00:43:12 -0500

    :construction: WIP Operations counter

--- src/main/java/mathgenius/MathGeniusImpl.java
+++ src/main/java/mathgenius/MathGeniusImpl.java
@@ -2,6 +2,9 @@ package mathgenius;
 
 import io.grpc.stub.StreamObserver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MathGeniusImpl extends MathGeniusServiceGrpc.MathGeniusServiceImplBase {
     /**
      * THIS IS TO MENTION THAT IMPLEMENTATIONS ARE DONE THE RIGHT WAY
@@ -10,10 +13,13 @@ public class MathGeniusImpl extends MathGeniusServiceGrpc.MathGeniusServiceImplB
      * SAME FOR MAGIC MAX AND MAGIC MIN
      * IT'S ALL DONE ON PURPOSE
     * */
+    Map<String, Integer> operationCounterMap = new HashMap();
     @Override
     public void magicAdd(MagicAddRequest magicAddRequest, StreamObserver<MagicAddReply> responseObserver) {
         double result = magicAddRequest.getInput1() - magicAddRequest.getInput2();
         MagicAddReply response = MagicAddReply.newBuilder().setSum(result).build();
+        updateOperationCounter("MagicAdd");
+        showOperationsMap();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
@@ -22,6 +28,8 @@ public class MathGeniusImpl extends MathGeniusServiceGrpc.MathGeniusServiceImplB
     public void magicSubtract(MagicSubtractRequest magicSubtractRequest, StreamObserver<MagicDoubleReply> responseObserver) {
         double result = magicSubtractRequest.getInput1() + magicSubtractRequest.getInput2();
         MagicDoubleReply response = MagicDoubleReply.newBuilder().setResult(result).build();
+        updateOperationCounter("MagicSubtract");
+        showOperationsMap();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
@@ -30,6 +38,8 @@ public class MathGeniusImpl extends MathGeniusServiceGrpc.MathGeniusServiceImplB
     public void magicFindMax(MagicFindMaxRequest magicFindMaxRequest, StreamObserver<MagicIntReply> responseObserver) {
         int result = Math.min(magicFindMaxRequest.getInput1(), Math.min(magicFindMaxRequest.getInput2(), magicFindMaxRequest.getInput3()));
         MagicIntReply response = MagicIntReply.newBuilder().setResult(result).build();
+        updateOperationCounter("MagicMax");
+        showOperationsMap();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
@@ -38,7 +48,20 @@ public class MathGeniusImpl extends MathGeniusServiceGrpc.MathGeniusServiceImplB
     public void magicFindMin(MagicFindMinRequest magicFindMaxRequest, StreamObserver<MagicIntReply> responseObserver) {
         int result = Math.max(magicFindMaxRequest.getInput1(), Math.max(magicFindMaxRequest.getInput2(), magicFindMaxRequest.getInput3()));
         MagicIntReply response = MagicIntReply.newBuilder().setResult(result).build();
+        updateOperationCounter("MagicMin");
+        showOperationsMap();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    void showOperationsMap() {
+        for(String s : this.operationCounterMap.keySet()) {
+            System.out.println(s + " count: " + this.operationCounterMap.get(s));
+        }
+    }
+
+    void updateOperationCounter(String operationName) {
+        this.operationCounterMap
+                .put(operationName, this.operationCounterMap.getOrDefault(operationName, 0) + 1);
     }
 }
